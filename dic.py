6426488f3f2fe6bb0c98a01a8fca98cd8f94ad3a
unknown <garima.in.nms@gmail.com>
2023-07-31 15:22:04 +0545

    python practice

---
+++ dic.py
@@ -0,0 +1,14 @@
+
+#dic
+
+myDic = {
+    "name":"garima",
+
+    "age" : 23,
+    "number" : 987654
+}
+
+a= myDic["name"]
+
+print(myDic)
+print(a)
--- list.py
+++ list.py
@@ -3,3 +3,16 @@ print(f"Hello{name}")
 
 mark= [20,30,40,40,25,35,98,78,68,57]
 print(mark[1])
+
+mark.append(10)
+print(mark)
+
+mark.clear()
+print(mark)
+
+mark.insert("1:int")
+print(mark)
+
+mark.remove(50)
+mark.pop(0)
+
